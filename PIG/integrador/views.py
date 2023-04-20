035d6ce445f95e832eeebc1d85269cbd7ad4b314
Jere Fassi <jeremiasfassi@gmail.com>
2023-04-20 00:41:41 -0300

    Herencia entre templates, configuracion de rutas, vistas y estaticos

--- PIG/integrador/views.py
+++ PIG/integrador/views.py
@@ -9,8 +9,128 @@ def index(request):
 
     return render(request,'integrador/index.html', context)
 
+def dashboard(request):
+    
+    context = {}
+
+    return render(request,'integrador/dashboard.html', context)
+
 def forms(request):
     
     context = {}
 
     return render(request,'integrador/forms.html', context)
+
+def tables(request):
+    
+    context = {}
+
+    return render(request,'integrador/tables.html', context)
+
+def charts(request):
+    
+    context = {}
+
+    return render(request,'integrador/charts.html', context)
+
+def icons(request):
+    
+    context = {}
+
+    return render(request,'integrador/icons.html', context)
+
+def ui_buttons(request):
+    
+    context = {}
+
+    return render(request,'integrador/ui-buttons.html', context)
+
+def ui_badges(request):
+    
+    context = {}
+
+    return render(request,'integrador/ui-badges.html', context)
+
+def ui_cards(request):
+    
+    context = {}
+
+    return render(request,'integrador/ui-cards.html', context)
+
+def ui_alerts(request):
+    
+    context = {}
+
+    return render(request,'integrador/ui-alerts.html', context)
+
+def ui_tabs(request):
+    
+    context = {}
+
+    return render(request,'integrador/ui-tabs.html', context)
+
+def ui_date_time_picker(request):
+    
+    context = {}
+
+    return render(request,'integrador/ui-date-time-picker.html', context)
+
+def login(request):
+
+    context = {}
+
+    return render(request,'integrador/login.html', context)
+
+def signup(request):
+
+    context = {}
+
+    return render(request,'integrador/signup.html', context)
+
+def forgot_password(request):
+
+    context = {}
+
+    return render(request,'integrador/forgot-password.html', context)
+
+def blank(request):
+
+    context = {}
+
+    return render(request,'integrador/blank.html', context)
+
+def error_404(request):
+
+    context = {}
+
+    return render(request,'integrador/error-404.html', context)
+
+def error_500(request):
+
+    context = {}
+
+    return render(request,'integrador/error-500.html', context)
+
+def users(request):
+
+    context = {}
+
+    return render(request,'integrador/users.html', context)
+
+def roles(request):
+
+    context = {}
+
+    return render(request,'integrador/roles.html', context)
+
+def permissions(request):
+
+    context = {}
+
+    return render(request,'integrador/permissions.html', context)
+
+def settings(request):
+
+    context = {}
+
+    return render(request,'integrador/settings.html', context)
