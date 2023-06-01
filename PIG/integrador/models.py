c7f96f8936ad1a82ffb461739a8abbe55fd93b90
AnabellaT <113066235+AnabellaT@users.noreply.github.com>
2023-05-31 23:54:37 -0300

    crear modelos y asociar empleados y servicios al admin

--- PIG/integrador/models.py
+++ PIG/integrador/models.py
@@ -1,27 +1,46 @@
 from django.db import models
 from datetime import datetime
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Persona(models.Model):
     nombre = models.CharField(max_length=100,verbose_name='Nombre')
     apellido = models.CharField(max_length=100,verbose_name='Apellido')
-    domicilio = models.CharField(max_length=100,verbose_name='Domicilio')
-    dni = models.IntegerField(verbose_name="DNI")
+    #domicilio = models.CharField(max_length=100,verbose_name='Domicilio')
+    DOC_TIPO = [
+        (1,'DNI'),
+        (2,'CUIT'),
+        (3, 'CUIL'),]
+    tipo_dni = models.IntegerField(choices=DOC_TIPO, verbose_name='Tipo de documento')
+    dni = models.IntegerField(verbose_name="Documento")
     email = models.EmailField(max_length=150)
     baja=models.BooleanField(default=False)
 
     class Meta:
         abstract = True
 
+class IntegerRangeField(models.IntegerField):
+    def __init__(self, verbose_name=None, name=None, min_value=None, max_value=None, **kwargs):
+        self.min_value, self.max_value = min_value, max_value
+        models.IntegerField.__init__(self, verbose_name, name, **kwargs)
+    def formfield(self, **kwargs):
+        defaults = {'min_value': self.min_value, 'max_value':self.max_value}
+        defaults.update(kwargs)
+        return super(IntegerRangeField, self).formfield(**defaults)
+
+PROVINCIA=[
+        (1, 'Buenos Aires'),
+        (2, 'Córdoba'),
+        (3, 'Santa Fe'),
+    ]
+
 class Empleado(Persona):
     legajo = models.IntegerField(verbose_name='Legajo')
-    CATEGORIA = [
-        (1,'Vendedor'),
-        (2,'Supervisor'),
-    ]
-    categoria = models.IntegerField(choices = CATEGORIA, verbose_name = 'Categoría')
-    comision = models.IntegerField(null = True, verbose_name = 'Comisión de Venta')
-        
+    #CATEGORIA = [
+        #(1,'Vendedor'),
+        #(2,'Supervisor'),]
+    #categoria = models.IntegerField(choices = CATEGORIA, verbose_name = 'Categoría')
+    
     def __str__(self):
         return f"{self.legajo} - {self.apellido}"
     
@@ -36,22 +55,31 @@ class Empleado(Persona):
     class Meta():
         verbose_name_plural = 'empleados'
 
+class Vendedor(Empleado):
+    comision = models.IntegerField(null = True, verbose_name = 'Comisión de Venta')
+    
+    class Meta():
+        verbose_name_plural = 'vendedores'
+
+class Tecnico(Empleado):
+    provincia = models.IntegerField(choices=PROVINCIA)
+
+class Supervisor(Empleado):
+    pass
+
 class Cliente(Persona):
-    SERVICIOS = [
-        (1,'Internet 300'),
-        (2,'Internet 500'),
-        (3,'Internet 1000'),
-        (4, 'Pymes 1000'),
-    ]
-    tipo_servicio = models.IntegerField(choices=SERVICIOS)
+    #SERVICIOS = [
+        #(1,'Internet 300'),
+        #(2,'Internet 500'),
+        #(3,'Internet 1000'),
+        #(4, 'Pymes 1000'),]
+    #tipo_servicio = models.IntegerField(choices=SERVICIOS)
     fecha_alta = models.DateField(verbose_name='Fecha de Alta')
     fecha_baja = models.DateField(null=True, blank=True)
-    coordenada_domicilio = models.CharField(max_length=50, verbose_name='Coordenadas domicilio')
-    legajo=models.ForeignKey(Empleado, on_delete=models.CASCADE, verbose_name='Vendedor asignado')
+    vendedor_id = models.ForeignKey(Vendedor, on_delete=models.CASCADE, verbose_name='Vendedor asignado')
     
-
     def __str__(self):
-        return f"{self.apellido} - {self.domicilio}"
+        return f"{self.apellido} - {self.dni}"
     
     def soft_delete(self):
         self.baja=True
@@ -63,3 +91,33 @@ class Cliente(Persona):
     
     class Meta():
         verbose_name_plural = 'clientes'
+
+class Servicio(models.Model):
+    descripcion = models.CharField(max_length=50, verbose_name='Tipo de servicio')
+    downstream_mv = models.IntegerField(validators=[MinValueValidator(-5),
+                                       MaxValueValidator(10)],default=0)
+    downstream_snr = models.IntegerField(validators=[MinValueValidator(33),
+                                       MaxValueValidator(40)],default=37)
+    upstream_mv = models.IntegerField(validators=[MinValueValidator(40),
+                                       MaxValueValidator(50)],default=45)
+
+class Domicilio(models.Model):
+    cliente_id = models.ForeignKey(Cliente, on_delete=models.CASCADE, verbose_name='Cliente')
+    servicio_id = models.ManyToManyField(Servicio, verbose_name='Tipo de servicio')
+    direccion = models.CharField(max_length=100, verbose_name='Domicilio')
+    codigo_postal = models.CharField(max_length=20, verbose_name='Código Postal')
+    localidad = models.CharField(max_length=50, verbose_name='Localidad')
+    provincia =models.IntegerField(choices=PROVINCIA)
+    latitud=models.FloatField(max_length=20, verbose_name='Coordenada Latitud')
+    longitud=models.FloatField(max_length=20, verbose_name='Coordenada Longitud')
+
+class OrdenTrabajo(models.Model):
+    domicilio_id=models.ForeignKey(Domicilio, on_delete=models.CASCADE, verbose_name='Domicilio de instalación')
+    tecnico_id=models.ForeignKey(Tecnico, on_delete=models.CASCADE, verbose_name='Técnico Asignado')
+    fecha_instalacion=models.DateField(verbose_name='Fecha de Instalación')
+    ESTADO= [
+        (1,'Pendiente'),
+        (2,'Realizada'),
+        (3,'Cancelada'),
+        ]
+    estado_ot=models.IntegerField(choices=ESTADO, verbose_name='Estado de la Orden de Trabajo')
